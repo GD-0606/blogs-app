97941b7bc097eea6de741b08f2c9188bdf38557d
ets-gopi <gopi.m@entitledarts.com>
2025-05-18 16:47:10 +0530

    mongo url changed

--- blogServer/db/connect.js
+++ blogServer/db/connect.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = () => {
   mongoose
-    .connect(
-      "mongodb+srv://gddev2022:060621@simpleshare-dashboard.yonui.mongodb.net/",
-      { dbName: "blogs" }
-    )
+    .connect("mongodb://localhost:27017/", { dbName: "blogs" })
     .then((res) => {
       console.log("connected");
     });
